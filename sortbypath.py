56ec49e6c0bc6938a76aa766e653332548d752c8
policarddavid <policarddavid@ufl.edu>
2024-11-13 20:57:29 -0500

    output to new csv file

--- sortbypath.py
+++ sortbypath.py
@@ -19,6 +19,8 @@ path_counts = Counter(row[2] for row in rows)
 sorted_rows = sorted(rows, key=lambda row: (path_counts[row[2]], row[2], row[1]))
 
 
-# Print sorted rows for verification
-for row in sorted_rows:
-	print(row)
+# Write sorted rows to a new CSV file
+output_file_path = './sorted_example.csv'
+with open(output_file_path, mode='w', newline='') as file:
+	writer = csv.writer(file)
+	writer.writerows(sorted_rows)
